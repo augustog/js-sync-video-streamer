fed3c31b40acd160fa5a0bc3ec1fc19f556c68ab
Augusto Gesualdi <augusto.gesualdi@gmail.com>
2017-02-19 22:20:02 -0300

    Refactor Player to hold video and controllers

--- dev/app/__tests__/Player-test.jsx
+++ dev/app/__tests__/Player-test.jsx
@@ -2,12 +2,31 @@
  * Created by augusto on 19/2/17.
  */
 
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import Player from '../Player.jsx'
 import {Play, Pause, FullScreen} from '../Controls.jsx';
 
 describe('<Player />', () => {
-    it('Should render a set of controls (play, pause, fullscreen)', ()=> {
-        expect(shallow(<Player />).contains(<Play />)).toBe(true);
-        expect(shallow(<Player />).contains(<Pause />)).toBe(true);
-        expect(shallow(<Player />).contains(<FullScreen />)).toBe(true);
+
+    const wrapper = shallow(<Player src="./test.mp4"/>);
+
+    it('Should render a video tag with class video & source tag according to src prop.', ()=>{
+        expect(wrapper.contains(
+            <video className="video">
+                <source src="./test.mp4" type="video/mp4" />
+                Your browser does not support video tags, get serious dude.
+            </video>
+                )).toBe(true);
+    })
+
+    it('Should render a set of controls (play, pause, fullscreen).', ()=> {
+        expect(wrapper.contains(<Play />)).toBe(true);
+        expect(wrapper.contains(<Pause />)).toBe(true);
+        expect(wrapper.contains(<FullScreen />)).toBe(true);
     });
+
+    console.log(wrapper.debug());
+
 });
--- dev/app/player.jsx
+++ dev/app/player.jsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import {Component} from 'react';
+
+import {Play, Pause, FullScreen} from './Controls.jsx';
 
 
 const states = {
@@ -12,20 +15,28 @@ class Player extends Component {
 
     constructor(props){
         super(props);
+
+        var srcComponents = props.src.split('.');
+
         this.state = {
             src: props.src,
+            srcType: srcComponents[srcComponents.length - 1],
             state: states.IDLE
         }
     }
 
     render(){
-        var videoType = this.state.src.split('.')[-1];
 
         return (
-            <video>
-                <source src="{this.props.src}" type="video/{videoType}" />
-                Your browser does not support video tags, get serious dude.
-            </video>
+            <div className="player-container">
+                <video className="video">
+                    <source src={this.state.src} type={"video/"+this.state.srcType} />
+                    Your browser does not support video tags, get serious dude.
+                </video>
+                <Play />
+                <Pause />
+                <FullScreen />
+            </div>
         );
 
     }
